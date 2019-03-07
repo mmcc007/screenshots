7a37d7ef11fe310be16f679ae2461ee3ec10ba09
Maurice McCabe <mmcc007@gmail.com>
2019-03-06 20:55:24 -0800

    Added back timeouts

--- lib/capture_screen.dart
+++ lib/capture_screen.dart
@@ -4,10 +4,11 @@ import 'dart:io';
 ///
 /// Called by integration test to capture images.
 ///
-Future screenshot(final driver, Map config, String name) async {
+Future screenshot(final driver, Map config, String name,
+    [Duration timeout = const Duration(seconds: 5)]) async {
   // todo: auto-naming scheme
   final stagingDir = config['staging'] + '/test';
-  await driver.waitUntilNoTransientCallbacks();
+  await driver.waitUntilNoTransientCallbacks(timeout: timeout);
   final List<int> pixels = await driver.screenshot();
   final File file =
       await File(stagingDir + '/' + name + '.png').create(recursive: true);
