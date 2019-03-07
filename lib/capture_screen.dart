00c976dad769dfd3fa4009a3c4538462e07f0712
Maurice McCabe <mmcc007@gmail.com>
2019-03-06 18:15:58 -0800

    Removed all timeouts (latest version of Flutter uses a timing and logging strategy) Added screen size definition to emulator

--- lib/capture_screen.dart
+++ lib/capture_screen.dart
@@ -4,11 +4,10 @@ import 'dart:io';
 ///
 /// Called by integration test to capture images.
 ///
-Future screenshot(final driver, Map config, String name,
-    [Duration timeout = const Duration(seconds: 5)]) async {
+Future screenshot(final driver, Map config, String name) async {
   // todo: auto-naming scheme
   final stagingDir = config['staging'] + '/test';
-  await driver.waitUntilNoTransientCallbacks(timeout: timeout);
+  await driver.waitUntilNoTransientCallbacks();
   final List<int> pixels = await driver.screenshot();
   final File file =
       await File(stagingDir + '/' + name + '.png').create(recursive: true);
