b844c84cf2eb3b48f0a57a3c8f15c6d2aa5dac15
Maurice McCabe <mmcc007@gmail.com>
2019-05-24 00:17:02 -0700

    Changed threshold for selecting black or white status bar

--- test/statusbar_color_test.dart
+++ test/statusbar_color_test.dart
@@ -1,4 +1,5 @@
 import 'package:screenshots/image_magick.dart';
+import 'package:screenshots/process_images.dart';
 import 'package:test/test.dart';
 
 //const kThreshold = 0.75;
@@ -6,11 +7,12 @@ import 'package:test/test.dart';
 
 main() {
   test('select black or white statusbar', () {
+//    final imagePath = './test/resources/0.png';
     final imagePath = './test/resources/0.png';
     final cropSize = '1242x42+0+0';
 //    print('pwd=${cmd('pwd', [])}');
     // if threshold exceeded select black
-    if (thresholdExceeded(imagePath, cropSize, 0.76)) {
+    if (thresholdExceeded(imagePath, kCrop, kThreshold)) {
       print('use black statusbar');
     } else {
       print('use white statusbar');
