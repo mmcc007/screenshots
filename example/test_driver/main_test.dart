8e271a36c3490be0a65fcf8bae0ef3ebb1fd764f
Maurice McCabe <mmcc007@gmail.com>
2019-06-03 18:43:26 -0700

    Fixed missing await on driver close

--- example/test_driver/main_test.dart
+++ example/test_driver/main_test.dart
@@ -20,7 +20,7 @@ void main() {
     });
 
     tearDownAll(() async {
-      if (driver != null) driver.close();
+      if (driver != null) await driver.close();
     });
 
     test('tap on the floating action button; verify counter', () async {
