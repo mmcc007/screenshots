35f7f3ac1caa808fbe9396abccb5604ef9cb8848
Maurice McCabe <mmcc007@gmail.com>
2019-07-01 01:30:33 -0700

    Modified to work in a local context

--- test/flutter_tools_test.dart
+++ test/flutter_tools_test.dart
@@ -1,20 +1,19 @@
+import 'package:screenshots/flutter_tools/lib/src/context_runner.dart';
 import 'package:screenshots/flutter_tools/lib/src/emulator.dart';
-import 'package:test_api/test_api.dart' hide TypeMatcher, isInstanceOf;
-import 'flutter_tools/test/src/context.dart';
-
-export 'package:test_api/test_api.dart'
-    hide TypeMatcher, isInstanceOf; // Defines a 'package:test' shim.
+import 'package:test/test.dart';
 
 main() {
   test('start flutter tools device daemon', () {});
 
   group('EmulatorManager', () {
-    testUsingContext('getEmulators', () async {
-      // Test that EmulatorManager.getEmulators() doesn't throw.
-      final List<Emulator> emulators =
-          await emulatorManager.getAllAvailableEmulators();
-      print('emulators=$emulators');
-      expect(emulators, isList);
+    test('getEmulators', () async {
+      await runInContext(() async {
+        // Test that EmulatorManager.getEmulators() doesn't throw.
+        final List<Emulator> emulators =
+            await emulatorManager.getAllAvailableEmulators();
+        print('emulators=$emulators');
+        expect(emulators, isList);
+      });
     });
   });
 }
