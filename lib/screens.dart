4bb7934f57e9295c8870350425c324fd8a535457
Maurice McCabe <mmcc007@gmail.com>
2019-07-06 23:20:41 -0700

    Cleanup

--- lib/screens.dart
+++ lib/screens.dart
@@ -3,33 +3,26 @@ import 'dart:convert';
 import 'package:yaml/yaml.dart';
 import 'package:resource/resource.dart';
 
-///
-/// Parse screens file.
-///
+/// Manage screens file.
 class Screens {
-  static const devicePath = 'resources/screens.yaml';
+  static const _screensPath = 'resources/screens.yaml';
   Map _screens;
 
-  ///
   /// Get screens yaml file from resources and parse.
-  ///
   Future<void> init() async {
-    final resource = Resource("package:screenshots/$devicePath");
+    final resource = Resource("package:screenshots/$_screensPath");
     String screens = await resource.readAsString(encoding: utf8);
-    _screens = loadYaml(screens) as Map;
+    _screens = loadYaml(screens);
   }
 
   /// Get screen information
   Map get screens => _screens;
 
-  ///
-  /// Get map of screen properties from screens yaml file
-  ///
+  /// Get screen properties for [deviceName].
   Map screenProps(String deviceName) {
     Map screenProps;
-
-    (screens as YamlNode).value.forEach((os, v) {
-      v.value.forEach((screenNum, _screenProps) {
+    screens.values.forEach((osScreens) {
+      osScreens.values.forEach((_screenProps) {
         if (_screenProps['devices'].contains(deviceName)) {
           screenProps = _screenProps;
         }
