535bf0a17b6147f7299d929a41a25f0d75c310a8
Maurice McCabe <mmcc007@gmail.com>
2019-07-09 19:32:16 -0700

    Cleanup

--- lib/src/capture_screen.dart
+++ lib/src/capture_screen.dart
@@ -7,11 +7,10 @@ import 'dart:io';
 Future screenshot(final driver, Map config, String name,
     {Duration timeout = const Duration(seconds: 30)}) async {
   // todo: auto-naming scheme
-  final stagingDir = config['staging'] + '/test';
   await driver.waitUntilNoTransientCallbacks(timeout: timeout);
   final List<int> pixels = await driver.screenshot();
-  final File file =
-      await File(stagingDir + '/' + name + '.png').create(recursive: true);
+  final stagingDir = '${config['staging']}/test';
+  final File file = await File('$stagingDir/$name.png').create(recursive: true);
   await file.writeAsBytes(pixels);
   print('Screenshot $name created');
 }
