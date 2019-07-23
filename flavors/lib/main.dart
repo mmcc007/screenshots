0f392050175f7a0448397cc60a8f312175ce3ae7
Maurice McCabe <mmcc007@gmail.com>
2019-07-22 20:20:53 -0700

    Added flavor example app and test #55

---
+++ flavors/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(Center(child: Flavor()));
+}
+
+class Flavor extends StatefulWidget {
+  @override
+  _FlavorState createState() => _FlavorState();
+}
+
+class _FlavorState extends State<Flavor> {
+  String _flavor;
+
+  @override
+  void initState() {
+    super.initState();
+    const MethodChannel('flavor')
+        .invokeMethod<String>('getFlavor')
+        .then((String flavor) {
+      setState(() {
+        _flavor = flavor;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: _flavor == null
+          ? const Text('Awaiting flavor...')
+          : Text(_flavor, key: const ValueKey<String>('flavor')),
+    );
+  }
+}
---
+++ flavors/test_driver/main.dart
@@ -0,0 +1,8 @@
+import 'package:flavors/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+  runApp(Center(child: Flavor()));
+}
