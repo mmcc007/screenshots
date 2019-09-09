23be8df0dd1a442497a036b6519ac6343c639e22
Maurice McCabe <mmcc007@gmail.com>
2019-09-09 13:13:44 -0700

    Modified findEmulator to match on id (which contains canonical name) instead of name (possible breaking change)

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:mockito/mockito.dart';
 import 'package:screenshots/screenshots.dart';
+import 'package:screenshots/src/daemon_client.dart';
+import 'package:screenshots/src/utils.dart';
 import 'package:test/test.dart';
 import 'package:tool_base/tool_base.dart';
 import 'package:tool_mobile/tool_mobile.dart';
@@ -16,24 +20,37 @@ class FakeAndroidSDK extends Fake implements AndroidSdk {
 
 main() {
   group('utils', () {
-    FakeAndroidSDK fakeAndroidSdk;
+    group('in context', () {
+      FakeAndroidSDK fakeAndroidSdk;
 
-    setUp(() {
-      fakeAndroidSdk = FakeAndroidSDK();
+      setUp(() {
+        fakeAndroidSdk = FakeAndroidSDK();
+      });
+
+      testUsingContext('get adb path', () async {
+        final adbPathFound = await isAdbPath();
+        expect(adbPathFound, isTrue);
+      }, overrides: <Type, Generator>{
+        AndroidSdk: () => fakeAndroidSdk,
+      });
+
+      testUsingContext('get emulator path', () async {
+        final emulatorPathFound = await isEmulatorPath();
+        expect(emulatorPathFound, isTrue);
+      }, overrides: <Type, Generator>{
+        AndroidSdk: () => fakeAndroidSdk,
+      });
     });
 
-    testUsingContext('get adb path', () async {
-      final adbPathFound = await isAdbPath();
-      expect(adbPathFound, isTrue);
-    }, overrides: <Type, Generator>{
-      AndroidSdk: () => fakeAndroidSdk,
-    });
-
-    testUsingContext('get emulator path', () async {
-      final emulatorPathFound = await isEmulatorPath();
-      expect(emulatorPathFound, isTrue);
-    }, overrides: <Type, Generator>{
-      AndroidSdk: () => fakeAndroidSdk,
+    group('not in context', () {
+      test('findEmulator', () {
+        final emulatorName = 'emulator name';
+        final emulatorId = '$emulatorName API 123'.replaceAll(' ', '_');
+        final expected = DaemonEmulator(
+            emulatorId, '$emulatorName version', 'category', 'platformType');
+        expect(
+            findEmulator([expected], emulatorName).name, equals(expected.name));
+      });
     });
   });
 }
