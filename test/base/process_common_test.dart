89c3736aaa2d19425f8787abb6b2e196d57c9aab
Maurice McCabe <mmcc007@gmail.com>
2019-09-23 13:57:33 -0700

    Fixed test

--- test/base/process_common_test.dart
+++ test/base/process_common_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 main() {
   group('process common', () {
     test('check executable path', () {
-      final flutterPath = getExecutablePath('flutter', '.');
+      final flutterPath = getExecutablePath('dart', '.');
       expect(flutterPath, isNotNull);
     });
   });
