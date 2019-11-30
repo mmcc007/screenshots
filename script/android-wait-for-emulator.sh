303e245cca9c3798642d16163d8b37bb9354ce93
Maurice McCabe <mmcc007@gmail.com>
2019-11-30 12:55:12 -0800

    Debug on cirrus

--- script/android-wait-for-emulator.sh
+++ script/android-wait-for-emulator.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Originally written by Ralf Kistner <ralf@embarkmobile.com>, but placed in the public domain
-#set -x
+
+set -x
 set +e
 
 bootanim=""
 failcounter=0
-#timeout_in_sec=360 # 6 minutes
-timeout_in_sec=600 # 10 minutes (for slow emulators in cloud)
+timeout_in_sec=360 # 6 minutes
 
 until [[ "$bootanim" =~ "stopped" ]]; do
   bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+echo bootanim=\`$bootanim\`
   if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
-    || "$bootanim" =~ "running" || "$bootanim" =~ "error: no emulators found" ]]; then
+    || "$bootanim" =~ "running" || "$bootanim" =~  "error: no emulators found" ]]; then
     let "failcounter += 1"
-    let "remaining = $timeout_in_sec - $failcounter"
-    echo "Waiting for emulator to start: $remaining of $timeout_in_sec : status: $bootanim"
-    if [[ $failcounter -gt timeout_in_sec ]]; then
+    echo "Waiting for emulator to start: $failcounter of $timeout_in_sec : status: $bootanim"
+    if [[ $failcounter -ge timeout_in_sec ]]; then
       echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
       exit 1
     fi
